dc91002bc26639564f3dd1fba7ab878ff072fb4a
ericwang994 <60726026+ericwang994@users.noreply.github.com>
2021-02-13 21:22:28 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,36 @@
 // Script.js
 
+let localStorage = window.localStorage;
+
+let cart = [];
+let productList = document.getElementById('product-list');
+let status = "";
+
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+
+  fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(json => {
+      if(localStorage.getItem('data') == null) {
+        localStorage.setItem('data', JSON.stringify(json))
+      }
+      
+      if (localStorage.getItem('cart') != null) {
+
+        cart = JSON.parse(localStorage.getItem('cart'));
+        document.getElementById('cart-count').textContent = cart.length;
+      } 
+      
+      for (let i = 0; i < json.length; i++) {
+        
+        if (cart.includes(json[i].id)) {
+          status = 'Remove from Cart';
+        }
+        else {
+          status = 'Add to Cart';
+        }
+        productList.appendChild(new ProductItem(json[i].id, json[i].title,
+          json[i].price, json[i].image, status))
+      }
+    })
 });
